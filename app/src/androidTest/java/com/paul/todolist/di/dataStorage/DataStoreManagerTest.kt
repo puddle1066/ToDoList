04be81db67973cfc07497a71a7dd5303620f9940
Paul <paull@inbox.com>
2023-08-25 15:28:33 +0100

    Added Storage Manager Test

---
+++ app/src/androidTest/java/com/paul/todolist/di/dataStorage/DataStoreManagerTest.kt
@@ -0,0 +1,21 @@
+package com.paul.todolist.di.dataStorage
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.test.core.app.ApplicationProvider
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class DataStoreManagerTest {
+    private val testPreferenceKey = stringPreferencesKey("TEST _STORE_VALUE")
+    private val testPreferenceValue = "AA"
+
+    @Test
+    fun testDataStore() = runBlocking() {
+        val store = DataStoreManager(ApplicationProvider.getApplicationContext())
+        store.storeValue(testPreferenceKey, testPreferenceValue)
+        store.readValue(testPreferenceKey) {
+            assertEquals(this, testPreferenceValue)
+        }
+    }
+}
