c318e1c732913d69b9f54d9565ff65d079260947
Paul <paull@inbox.com>
2023-11-18 14:03:44 +0000

    added carousel screens

--- app/src/main/java/com/paullanducci/todolist/ToDoList.kt
+++ app/src/main/java/com/paullanducci/todolist/ToDoList.kt
@@ -3,7 +3,6 @@ package com.paullanducci.todolist
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-
 @HiltAndroidApp
 class ToDoList : Application() {
 
---
+++ app/src/main/java/com/paullanducci/todolist/ui/main/tutorial/TutorialContainer.kt
@@ -0,0 +1,47 @@
+package com.paullanducci.todolist.ui.main.tutorial
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerSnapDistance
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.runtime.Composable
+
+import com.paullanducci.todolist.ui.main.tutorial.pages.screen_1
+import com.paullanducci.todolist.ui.main.tutorial.pages.screen_2
+import com.paullanducci.todolist.ui.main.tutorial.pages.screen_3
+import com.paullanducci.todolist.ui.main.tutorial.pages.screen_last
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun tutorialContainer(pagerState: PagerState, count: Int) {
+
+    val fling = PagerDefaults.flingBehavior(
+        state = pagerState,
+        pagerSnapDistance = PagerSnapDistance.atMost(count)
+    )
+
+    HorizontalPager(
+        state = pagerState,
+        beyondBoundsPageCount = count,
+        flingBehavior = fling
+    ) { page ->
+        when (page) {
+            0 -> {
+                screen_1()
+            }
+
+            1 -> {
+                screen_2()
+            }
+
+            2 -> {
+                screen_3()
+            }
+
+            3 -> {
+                screen_last()
+            }
+        }
+    }
+}
