c8c3cca28589054affc01e70f4a1c7e9ad3461ea
Paul <paull@inbox.com>
2024-06-08 17:29:58 +0100

    Set Status bar and nav bar colors

--- app/src/main/java/com/paullanducci/todolist/ui/theme/Theme.kt
+++ app/src/main/java/com/paullanducci/todolist/ui/theme/Theme.kt
@@ -1,10 +1,15 @@
 package com.paullanducci.todolist.ui.theme
 
+import android.R
+import android.app.Activity
+import android.graphics.Color
+import android.view.Window
+import android.view.WindowManager
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 
 
 @Composable
@@ -17,7 +22,6 @@ fun ToDoListTheme(
         else -> LightColorScheme
     }
 
-    var barColor = MaterialTheme.colorScheme.primary
 
     MaterialTheme(
         colorScheme = selectedColorScheme,
@@ -26,28 +30,13 @@ fun ToDoListTheme(
         content = content
     )
 
-    val systemUiController = rememberSystemUiController()
-
-    SideEffect {
-        systemUiController.setStatusBarColor(
-            color = barColor,
-            darkIcons = darkTheme
-        )
-    }
-
-    SideEffect {
-        systemUiController.setNavigationBarColor(
-            color = barColor,
-            darkIcons = darkTheme
-        )
-    }
-
-    SideEffect {
-        systemUiController.setSystemBarsColor(
-            color = barColor,
-            darkIcons = darkTheme
-        )
-    }
+    val activity = LocalContext.current as Activity
+    val window: Window = activity.window
+    val color = selectedColorScheme.background.toArgb()
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+    window.statusBarColor = color
+    window.navigationBarColor = color
 
 }
 
